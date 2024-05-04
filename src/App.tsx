a5ad3a3ac6ebe418dc752d3eb2b1bfab803b3e54
saksham <saksham@genflowly.com>
2024-05-04 16:35:39 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
+import MiniDrawer from './components/SideBar';
+
+function App() {
+  return (
+ <div></div>
+  );
+}
+
+export default App;
